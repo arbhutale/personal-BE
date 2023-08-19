60d31c54f7d2185dd4d705ecdbd352f15a6223fe
anilbhutale <anil.bhutale@hedgehoglab.com>
2023-08-20 01:37:18 +0530

    added user view

--- api/users/views.py
+++ api/users/views.py
@@ -12,7 +12,7 @@ from rest_framework import serializers
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = get_user_model()
-        fields = ('id', 'username', 'token')
+        fields = '__all__'
 
 
 class UserAPIView(RetrieveAPIView):
